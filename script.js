af79826bc9efb60aad839666f82504b2bea21ace
Khuthumira <khuthumirabusentr@gmail.com>
2025-03-30 18:48:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 function checkLogin(event) {
     event.preventDefault(); // Prevent form from refreshing page
     
